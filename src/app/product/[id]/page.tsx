6e4ded49646c9291fe05ebe7dfae768f1ae49176
abdul rehman <abdulrehmanmuglll00@gmail.com>
2026-07-20 12:49:35 +0500

    testing  image issues

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -48,6 +48,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
             height={900}
             className={Styles.image}
             priority
+            unoptimized
           />
         </div>
 
